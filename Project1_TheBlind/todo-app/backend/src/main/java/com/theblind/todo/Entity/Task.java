b90a74d4248f198e827ff0de396dd31e71bce792
Eric Adcock <cireadcock@gmail.com>
2026-06-18 12:26:34 -0400

    Add annotation to prevent Task entity from creating circular references when converted to string, used in comparisons, or stringified into JSON

--- Project1_TheBlind/todo-app/backend/src/main/java/com/theblind/todo/Entity/Task.java
+++ Project1_TheBlind/todo-app/backend/src/main/java/com/theblind/todo/Entity/Task.java
@@ -4,6 +4,8 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -15,7 +17,9 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Data
 @NoArgsConstructor
@@ -29,16 +33,27 @@ public class Task {
     @Column(name = "task_content", length = 50)
     private String taskContent;
 
+    @Column(name = "parent_task_id")
+    private UUID parent_task_id;
+
     // Self-referencing FK: a task can have a parent task
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    @JsonBackReference
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "parent_task_id")
+    @JoinColumn(name = "parent_task_id", referencedColumnName = "id", insertable = false, updatable = false)
     private Task parentTask;
 
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    @JsonManagedReference
     @OneToMany(mappedBy = "parentTask", fetch = FetchType.LAZY)
     private List<Task> subtask;
 
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", nullable = false)
+    @JoinColumn(name = "user_id")
     private User user;
 
     @Column(name = "is_complete")
